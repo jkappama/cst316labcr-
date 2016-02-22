726bfd0606f1ed3eb3e767520225e093b801f574
Joshva Kappamamoottil <jkappama@asu.edu>
2016-02-22 10:34:01 -0800

    fixed issue 9

--- crlab_given/given/src/banking/gui/Main.java
+++ crlab_given/given/src/banking/gui/Main.java
@@ -19,7 +19,7 @@ final class Main {
 	 * @param args command-line arguments
 	 * @throws Exception as per typical main specifications
 	 */
-	public static void main(final String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
 		if (args.length != 1) {
 			System.out.println("Usage: java FormMain <property file>");
